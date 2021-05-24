9e10071353711589874c96659872f83410c58f9c
Ryan <ryan.bush1014@gmail.com>
2021-05-24 14:04:01 -0400

    First commit--George Radojcic's website

---
+++ scripts/fade.js
@@ -0,0 +1,33 @@
+let figs = document.getElementsByTagName("figure");
+let secs = document.getElementsByClassName("section");
+let lines = document.getElementsByTagName("hr");
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.05
+}
+
+let callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove("invis");
+            entry.target.classList.add("appeared");
+        }
+    });
+};
+
+
+
+let observer = new IntersectionObserver(callback, options);
+
+function addObserver(elems) {
+    for(let i = 0; i < elems.length; ++i) {
+        elems[i].classList.add("invis");
+        observer.observe(elems[i]);
+    }
+}
+
+addObserver(figs);
+addObserver(secs);
+addObserver(lines);

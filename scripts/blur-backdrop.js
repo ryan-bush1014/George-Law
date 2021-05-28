58149f5c5cb1ca8acc5597c16a3fbef2d70e760c
Ryan <ryan.bush1014@gmail.com>
2021-05-28 18:06:53 -0400

    removed css support func

--- scripts/blur-backdrop.js
+++ scripts/blur-backdrop.js
@@ -1,10 +1,3 @@
-// https://stackoverflow.com/questions/36191797/how-to-check-if-css-value-is-supported-by-the-browser
-function cssPropertyValueSupported(prop, value) {
-    var d = document.createElement('div');
-    d.style[prop] = value;
-    return d.style[prop] === value;
-}
-
 if (CSS.supports('backdrop-filter', 'blur(10px)')) {
     console.log("CSS \'backdrop-filter\' supported - upgraded styles implemented");
     document.querySelector(".hamburger-container").classList.add("blur");
